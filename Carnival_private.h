9f6686547ea4b564c6617e399796b587a7c42383
William Pascal <william_pascal@outlook.com>
2023-02-17 09:04:32 +0800

    Modified gitignore
    Ignore Description.

--- Carnival_private.h
+++ Carnival_private.h
@@ -5,19 +5,19 @@
 #define CARNIVAL_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.0.0.295"
+#define VER_STRING	"0.0.0.296"
 #define VER_MAJOR	0
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	295
+#define VER_BUILD	296
 #define COMPANY_NAME	""
-#define FILE_VERSION	"0.0.0.295"
+#define FILE_VERSION	"0.0.0.296"
 #define FILE_DESCRIPTION	"[Carnival - STUDY - C]"
 #define INTERNAL_NAME	"Carnival_MinGW"
 #define LEGAL_COPYRIGHT	"GPL 3.0"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"Carnival"
-#define PRODUCT_VERSION	"0.0.0.295"
+#define PRODUCT_VERSION	"0.0.0.296"
 
 #endif /*CARNIVAL_PRIVATE_H*/
